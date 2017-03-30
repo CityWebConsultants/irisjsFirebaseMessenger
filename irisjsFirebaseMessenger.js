b746294e95d949e64a8c669f79f8f9524aeeb53d
Time Lord <chito.cascante@gmail.com>
2017-03-30 15:45:53 +0800

    add seeting page for server key

--- irisjsFirebaseMessenger.js
+++ irisjsFirebaseMessenger.js
@@ -1,32 +1,8 @@
 var request = require("request");
-/*
-  Hook to show the page for modifying the mysql info
+
+/**
+ * Global function Send notification
  */
-
-iris.modules.irisjsFirebaseMessenger.globals.renderMessageForm = function (thisHook, data) {
-
-  data.schema.serverKey = {
-    "type": "text",
-    "title": "Server Key",
-    "required": true,
-    "default":  ""
-  };
-  data.schema.recipientToken = {
-    "type": "text",
-    "title": "Recipient Token",
-    required: true,
-    "default": ""
-  };
-  data.schema.message = {
-    "type": "text",
-    "title": "Message",
-    "required": true,
-    "default": ""
-  };
-
-  thisHook.pass(data);
-};
-
 iris.modules.irisjsFirebaseMessenger.globals.sendNotification = function (thisHook,params) {
 
   request({
@@ -53,21 +29,136 @@ iris.modules.irisjsFirebaseMessenger.globals.sendNotification = function (thisHo
 
 };
 
+/**
+ * Message Form
+ */
+iris.modules.irisjsFirebaseMessenger.globals.renderMessageForm = function (thisHook, data) {
+
+  data.schema.recipientToken = {
+    "type": "text",
+    "title": "Recipient Token",
+    required: true,
+    "default": ""
+  };
+  data.schema.message = {
+    "type": "text",
+    "title": "Message",
+    "required": true,
+    "default": ""
+  };
+
+  thisHook.pass(data);
+};
+
+/**
+ * register hook render message form
+ */
 iris.modules.irisjsFirebaseMessenger.registerHook("hook_form_render__message", 0, function (thisHook, data) {
 
   iris.modules.irisjsFirebaseMessenger.globals.renderMessageForm(thisHook, data);
 
 });
 
+/**
+ * register hook submit message form
+ */
 iris.modules.irisjsFirebaseMessenger.registerHook("hook_form_submit__message", 0, function (thisHook, data) {
 
-  iris.modules.irisjsFirebaseMessenger.globals.sendNotification(thisHook, thisHook.context.params);
+  iris.readConfig('firebase', 'setting').then(function (config) {
+    
+    thisHook.context.params.serverKey = config.serverKey;
+    iris.modules.irisjsFirebaseMessenger.globals.sendNotification(thisHook, thisHook.context.params);
+
+  }, function (fail) {
+    
+    iris.modules.irisjsFirebaseMessenger.globals.sendNotification(thisHook, thisHook.context.params);
+
+  });
+  
 
 });
 
+/**
+ * Message Form
+ */
+iris.modules.irisjsFirebaseMessenger.globals.renderSettingForm = function (thisHook, data, config) {
+  
+  data.schema.serverKey = {
+    "type": "text",
+    "title": "Server Key",
+    "required": true,
+    "default": config.serverKey ? config.serverKey : ""
+  };
+
+  thisHook.pass(data);
+};
+
+/**
+ * register hook render setting form
+ */
+iris.modules.irisjsFirebaseMessenger.registerHook("hook_form_render__setting", 0, function (thisHook, data) {
+
+  iris.readConfig('firebase', 'setting').then(function (config) {
+    iris.modules.irisjsFirebaseMessenger.globals.renderSettingForm(thisHook, data, config);
+
+  }, function (fail) {
+    iris.modules.irisjsFirebaseMessenger.globals.renderSettingForm(thisHook, data, false);
+
+  });
+
+});
+
+/**
+ * register hook submit setting form
+ */
+iris.modules.irisjsFirebaseMessenger.registerHook("hook_form_submit__setting", 0, function (thisHook, data) {
+
+  iris.saveConfig(thisHook.context.params, 'firebase', 'setting');
+  thisHook.pass(data);
+
+});
+
+/**
+ * route to message form
+ */
 iris.route.get("/firebase/message", {}, function (req, res) {
 
   iris.modules.frontend.globals.parseTemplateFile(["message"], ['admin_wrapper'], {
+    'current': req.irisRoute.options,
+  }, req.authPass, req).then(function (success) {
+
+    res.send(success);
+
+  }, function (fail) {
+
+    iris.modules.frontend.globals.displayErrorPage(500, req, res);
+
+    iris.log("error", fail);
+
+  });
+
+});
+
+var menus = {
+  "firebaseadmin": {
+    "title": "Firebase setting",
+    "description": "Administer firebase server details.",
+    "permissions": ["can administer firebase"],
+    "menu": [{
+      menuName: "admin_toolbar",
+      parent: "/admin/config/firebase",
+      title: "Firebase settings"
+    }]
+  },
+};
+
+/*
+  Hook to show the page for modifying the mysql info
+ */
+iris.route.get("/admin/config/firebase", menus.firebaseadmin, function (req, res) {
+
+
+  iris.modules.frontend.globals.parseTemplateFile(["setting"], ['admin_wrapper'], {
     'current': req.irisRoute.options,
   }, req.authPass, req).then(function (success) {
 
